75335b4b57e58b5e7badeaef73dfcaf80ab001f1
Engi <pikastar43@gmail.com>
2022-12-30 12:18:36 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -12,16 +12,20 @@
 // // var変数は再宣言可能
 // var val1 = "再宣言";
 // console.log(val1);
- 
- let val2 = "let変数";
- console.log(val2);
 
- // let変数は上書き可能
- val2 = "上書き";
- console.log(val2);
+let val2 = "let変数";
+console.log(val2);
 
- // let変数は再宣言不可
- //let val2 = "再宣言";
- 
+// let変数は上書き可能
+val2 = "上書き";
+console.log(val2);
 
+// let変数は再宣言不可
+//let val2 = "再宣言";
 
+const val4 = {
+  name: "じゅげむ",
+  age: 99,
+};
+val4.name = "jyugemu";
+console.log(val4);
